5eb6599ba421dd2fbb4f068d73ae6f23ace5f896
Bacra Woo <bacra.woo@gmail.com>
2018-09-03 19:22:41 +0800

    init

---
+++ flow/pkghandler.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var debug = require('debug')('clientlinker-flow-pkghandler');
+
+exports = module.exports = function pkghandler(runtime, callback)
+{
+	var client = runtime.client;
+	var mod = initClient(client);
+
+	if (!mod) return callback.next();
+	var handler = mod[runtime.method];
+
+	if (handler)
+	{
+		return handler(runtime.query, runtime.body, callback, runtime.options);
+	}
+	else
+	{
+		debug('pkg no handler:%s', runtime.method);
+		return callback.next();
+	}
+};
+
+
+exports.initClient = initClient;
+function initClient(client)
+{
+	var options = client.options;
+	if (!options.pkghandler) return;
+
+	if (!client.pkghandlerModuleLoaded)
+	{
+		client.pkghandlerModule = require(options.pkghandler);
+		client.pkghandlerModuleLoaded = true;
+	}
+
+	return client.pkghandlerModule;
+}
